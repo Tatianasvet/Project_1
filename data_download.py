e2c6a0777d4ffe7fb84d53a1890b7dc379a6ec61
Tatianasvet <168006070+Tatianasvet@users.noreply.github.com>
2024-12-30 22:42:54 +0500

    average_price

--- data_download.py
+++ data_download.py
@@ -10,3 +10,9 @@ def fetch_stock_data(ticker, period='1mo'):
 def add_moving_average(data, window_size=5):
     data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
     return data
+
+
+def calculate_and_display_average_price(data):
+    average_price = data['Close'].mean(axis=0)
+    print(f'Среднее значение колонки "Close": {average_price}')
+    return data
